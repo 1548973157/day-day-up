cd5ef4d26eb93dbb96f13d37bfc8fd3d7925ec71
ZYL <ZYL@HHXX.COM>
2025-07-23 21:21:24 +0800

    头文件定义函数并在主函数调用

---
+++ C语言复习/头文件定义函数/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "myTool.h"  // 修改：使用引号而不是尖括号
+
+int main() {
+    int c = myAdd(9, 10);
+    printf("%d\n", c);
+    return 0;
+}
---
+++ C语言复习/头文件定义函数/myTool.c
@@ -0,0 +1,5 @@
+#include "myTool.h"  // 包含对应的头文件
+
+int myAdd(int a, int b) {
+    return a + b;
+}
